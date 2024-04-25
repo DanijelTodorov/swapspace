bbd4fe8b123e9d870c8a029579baba2b0c7daa8d
StojanMarinkovic <spiderdev999@gmail.com>
2024-04-25 22:22:31 +0900

    add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 const Header = () => {
   return (
-  <div className="header fixed text-white h-[100px] flex items-center px-[200px]">
+  <div className="header fixed text-white h-[100px] w-full flex items-center px-[200px] justify-between">
     <label className="text-[2rem] text-yellow-200">SWAPSPACE</label>
-
+    <div className="flex items-center gap-4">
+        <label>Products</label>
+        <label>Business</label>
+        <label>Support</label>
+        <label>About</label>
+        <label>Explore</label>
+    </div>
   </div>);
 };
 
